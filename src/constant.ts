9f544ed26951cb1ef688edb412f32c49001f15e8
zoo_cli <zoo_cli@gmail.com>
2025-08-12 18:24:37 +0800

    add constant

---
+++ src/constant.ts
@@ -0,0 +1,12 @@
+export const recentUpdateAnime = [
+    "我們不可能成為戀人！絕對不行。 (※似乎可行？)[06]",
+    "GRAND BLUE 碧藍之海 第二季[06]",
+    "Summer Pockets[19]",
+    "鬼人幻燈抄[18]",
+    "明天，美食廣場見。[06]",
+    "轉生成貓咪的大叔[44]",
+    "殺手旅店[06]",
+    "Princession Orchestra[17]",
+    "Gachiakuta[05]",
+    "醜男真戰士[06]",
+]
