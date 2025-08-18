8b05c096c546635909e3a9431f7d8c28c7f61e20
i321i <i321ixd@gmail.com>
2025-08-18 19:29:57 +0800

    New AnimeList

--- src/components/AnimeItem.tsx
+++ src/components/AnimeItem.tsx
@@ -1,9 +1,8 @@
 import "./AnimeItem.css"
 import { recentUpdateAnime } from "../constant";
 interface AnimeItemProps {
-    cAnime?: string
+    title?: string
 }
-export const AnimeItem: React.FC<AnimeItemProps> = (prop) => {
-    const { cAnime } = prop
-    return <div id="AnimeItem"> {cAnime}</div>
+export const AnimeItem: React.FC<AnimeItemProps> = ({ title }) => {
+    return <div id="AnimeItem"> {title}</div>
 }
--- src/components/AnimeList.tsx
+++ src/components/AnimeList.tsx
@@ -3,26 +3,22 @@ import "./AnimeList.css"
 import i from "./Component2";
 import { recentUpdateAnime } from "../constant";
 import { AnimeItem } from "./AnimeItem";
+import { useState } from "react";
 
 export const AnimeList = () => {
+  const [count, setCount] = useState(8)
+  const [word, setWord] = useState("hello,")
   const result = recentUpdateAnime.map((anime, index) => {
     return <div style={{ color: `#${index}${index}${index}` }}>{anime != null && anime} </div>
   })
-  const animeList = (recentUpdateAnime: any) => {
-    let list = []
-    for (let index = 0; index < recentUpdateAnime.length; index++) {
-      list.push(<AnimeItem cAnime={recentUpdateAnime[index]} />)
-    }
-    return list
-  }
-  
+  const animeList = recentUpdateAnime.map((anime) => <AnimeItem title={anime} />)
   //onst separateAnimeItem = 
   return (
     <div id="firstDiv">
+      {count + word}
       {result}
-      {animeList(recentUpdateAnime)}
-      {/* <AnimeItem cAnime={recentUpdateAnime[2]}>
-      </AnimeItem> */}
+      {animeList}
+      <button onClick={() => {setCount(count + 1); setWord(word + "world" )}}></button>
     </div>
   )
 }
