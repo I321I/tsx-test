f60c056da4b1a052e8549db8010c9c971c29503b
i321i <i321ixd@gmail.com>
2025-10-17 19:51:24 +0800

    mockData

--- src/components/PetitionList.tsx
+++ src/components/PetitionList.tsx
@@ -1,9 +1,24 @@
 import { useEffect, useState, type JSX, type JSXElementConstructor } from "react"
 import { data, useActionData } from "react-router"
-
+import Table from 'react-bootstrap/Table';
 interface PotitionListProps {
     title?: string
     onClick?: () => void
+}
+
+interface TableProps<T extends object> {
+    columns: (keyof T)[]
+    data: T[]
+}
+
+const mockData: TableProps<{ "屬性": number; "薩爾達": number; "評價": number; "長度": number; 123: number }> = {
+    columns: ["屬性", "薩爾達", "評價", "長度", 123],
+    data: [
+        { "屬性": 1, "薩爾達": 2, "評價": 3, "長度": 4, 123: 123 },
+        { "屬性": 4, "薩爾達": 3, "評價": 2, "長度": 1, 123: 123 },
+        { "屬性": 1, "薩爾達": 2, "評價": 4, "長度": 3, 123: 123 },
+        { "屬性": 1, "薩爾達": 3, "評價": 2, "長度": 4, 123: 123 },
+    ]
 }
 
 const petitionItem = [
@@ -27,7 +42,7 @@ export const PetitionList: React.FC<PotitionListProps> = ({ title, onClick }) =>
     const reg = /.+\/(.+)/
     const pList = pItem.map((item) =>
         <button type="button" className="btn btn-link"
-            onClick={() => { setUrl(item.url), setGetData(()=>item.getData) }}>
+            onClick={() => { setUrl(item.url), setGetData(() => item.getData) }}>
             {reg.exec(item.url)?.[1]}
         </button>
     )
@@ -39,9 +54,37 @@ export const PetitionList: React.FC<PotitionListProps> = ({ title, onClick }) =>
             setData(getData(data))
         })()
     }, [url])
+
+    const tableHead =
+        <tr>
+            {mockData.columns.map((item) =>
+                <th>{item}</th>
+            )}
+        </tr>
+
+    const tableBody = mockData.data.map((item) => {
+        return (
+            <tr>
+                {mockData.columns.map((column) =>
+                    <td>{item[column]}</td>
+                )}
+            </tr>)
+    })
+
+
+
+
     return (
         <div className="d-flex flex-column">
             {pList}
+            <Table bordered hover>
+                <thead>
+                    {tableHead}
+                </thead>
+                <tbody>
+                    {tableBody}
+                </tbody>
+            </Table>
             <div className="d-flex pt-4">
                 {JSON.stringify(data)}
             </div>
