57d9b7833476cf9c2340c05a4a1d3feeb6c753e7
i321i <i321ixd@gmail.com>
2026-01-10 08:55:33 +0000

    CSS

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,9 +1,12 @@
 import type React from "react"
+import styles123 from "./ToDoList.module.css"
 
 export const ToDoList: React.FC = () => {
-    return  (
-        <div>asd
-
+    return (
+        <div className={styles123.background + " " + "background mx-auto position-absolute top-5 start-0"}>
+            <div className={styles123.ToDoList + " " + "ToDoList"}>
+                123
+            </div>
         </div>
     )
 }
