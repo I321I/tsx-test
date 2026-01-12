202c586ac227657978388c135842137ca5595772
i321i <i321ixd@gmail.com>
2026-01-12 12:07:26 +0000

    inputProp

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,11 +1,20 @@
 import type React from "react"
 import styles123 from "./ToDoList.module.css"
+import { Form, InputGroup } from "react-bootstrap"
+
+const InputProp: React.FC = () => {
+    return (
+        <input className="position-relative"></input>
+    )
+}
 
 export const ToDoList: React.FC = () => {
+
     return (
         <div className={styles123.background + " " + "background mx-auto position-absolute top-5 start-0"}>
             <div className={styles123.ToDoList + " " + "ToDoList"}>
-                123
+                <InputProp></InputProp>
+                <InputProp></InputProp>
             </div>
         </div>
     )
