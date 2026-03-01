029a29fb4dc8f901befafc726596d88a792687bf
i321i <i321ixd@gmail.com>
2026-03-01 13:37:30 +0000

    lazy loading

--- src/components/Navigation.const.tsx
+++ src/components/Navigation.const.tsx
@@ -1,14 +1,15 @@
+import { lazy } from 'react'
+const AnimeList = lazy(() => import("./AnimeList").then(module => ({ default: module.AnimeList })))
+const EmptyContent = lazy(() => import("./EmptyContent"))
+const PetitionList = lazy(() => import("./PetitionList").then(module => ({ default: module.PetitionList })))
+const InputField = lazy(() => import("./InputField").then(module => ({ default: module.InputField })))
+const Counter = lazy(() => import("./Counter").then(module => ({ default: module.Counter })))
+const UrlFetch = lazy(() => import("./UrlFetch").then(module => ({ default: module.UrlFetch })))
+const UrlDataDisplay = lazy(() => import("./UrlDataDisplay").then(module => ({ default: module.UrlDataDisplay })))
+const ToDoList = lazy(() => import("./ToDoList_redux").then(module => ({ default: module.ToDoList })))
+const ToDoListAPI = lazy(() => import("./ToDoList_API").then(module => ({ default: module.ToDoListAPI })))
+const ToDoListMiddleware = lazy(() => import("./ToDoList_middleware").then(module => ({ default: module.ToDoListMiddleware })))
 
-import { AnimeList } from "./AnimeList";
-import EmptyContent from "./EmptyContent";
-import { PetitionList } from "./PetitionList";
-import { InputField } from "./InputField";
-import { Counter } from "./Counter";
-import { UrlFetch } from "./UrlFetch";
-import { UrlDataDisplay } from "./UrlDataDisplay";
-import { ToDoList } from "./ToDoList_redux";
-import { ToDoListAPI } from "./ToDoList_API";
-import { ToDoListMiddleware } from "./ToDoList_middleware";
 
 export const navigationConfig = [
     { to: "/", label: "App", element: () => <AnimeList /> },
